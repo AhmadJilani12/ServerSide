00e545f5800de72cf911ff26d28e2cf85a534f16
My8921 <ahmadali43a5@gmail.com>
2026-02-21 19:30:36 +0500

    updated changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require('../models/Product');
 const Category = require('../models/Category');
 const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 const path = require('path');
 const mongoose = require('mongoose');
 
